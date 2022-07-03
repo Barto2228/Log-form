d0fe67f24418508900081bb9d090a54f4cf47570
Barto2228 <89597497+Barto2228@users.noreply.github.com>
2022-07-03 11:57:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // zdefiniowanie funkcji
 function  sendForm(){
-    // utworzenie zmiennych name i  age i z htmlela otrzymujemy wartość
+    // utworzenie zmiennych email i  log i z htmlela otrzymujemy wartość
     let email = document.getElementById("email").value;
     let log = document.getElementById("log");
     // powitanje użykownika
